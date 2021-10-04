8cfc5744e520979268b2bd4f4f441384736663d6
Brendan Francis <B.Francis@SprengerV.com>
2021-10-04 06:44:16 -0400

    reorganizing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Dragon } = require('./lib/classes/Dragon');
-const { BreedCycle } = require('./lib/classes/BreedCycle');
+const { Dragon } = require('./lib/Dragon');
+const { BreedCycle } = require('./lib/BreedCycle');
 
 const BlazeUtiss = new Dragon('Blaze Utiss', 8, 7, 15, 0);
 const SirrushDutag = new Dragon('Sirrush Dutag', 12, 8, 10);
