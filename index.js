fa9e16b1e58d12ccb94dc6cd35660bf32c8ec5a5
Brendan Francis <B.Francis@SprengerV.com>
2021-10-15 20:36:42 -0400

    tabulate cumulative cost with each breed cycle and display total at the end

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { Dragon } = require('./lib/Dragon');
 const { BreedCycle } = require('./lib/BreedCycle');
+const { Calculate } = require('./lib/Calculate');
 
 const BlazeUtiss = new Dragon('Blaze Utiss', 8, 7, 15, 0);
 const SirrushDutag = new Dragon('Sirrush Dutag', 12, 8, 10);
-const ChoryrthFinola = new Dragon('Choryrth Finola', 7, 14, 9)
+const ChoryrthFinola = new Dragon('Choryrth Finola', 7, 14, 9);
 
-const breedCycle = new BreedCycle(BlazeUtiss, SirrushDutag, ChoryrthFinola);
-console.log('Results: ' + breedCycle.results);
-console.log('Total cost: ' + breedCycle.cost);
+const calculate = new Calculate(BlazeUtiss, SirrushDutag, ChoryrthFinola, 3, 'Rare');
+console.log(`Total: ${calculate.total} CYT`);
